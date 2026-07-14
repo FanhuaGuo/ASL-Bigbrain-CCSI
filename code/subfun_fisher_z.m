cce37e894a4107a04b59b497d386e316db5586b3
FanhuaGuo <110100356+FanhuaGuo@users.noreply.github.com>
2026-07-14 09:25:44 -0700

    Delete code directory

--- code/subfun_fisher_z.m
+++
@@ -1,6 +0,0 @@
-function [ fisher_z ] = subfun_fisher_z( r )
-
-fisher_z = log( (1+r) ./ (1-r) )/2;
-
-end
-
--- code/subfun_fisher_z_inverse.m
+++
@@ -1,6 +0,0 @@
-function [ r ] = subfun_fisher_z_inverse( z )
-
-r = (exp(2 .* z) - 1) ./ (exp(2 .* z) + 1);
-
-end
-
